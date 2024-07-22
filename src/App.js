a07c9147d64a5db74c14426b72d73a8b3e32da91
kushagra0333 <kushagrapandey0333@gmail.com>
2024-07-23 00:01:51 +0530

    fourth commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/header";
-import Home from './pages/home';
+import Home from "./pages/home"
 function App() {
   return (
     <div className="App">
