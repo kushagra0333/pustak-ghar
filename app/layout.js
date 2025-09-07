48f232ee8d245b4ecf6d37fffdb01c5e449f1870
Syeda Umaima <syedaumaima07@gmail.com>
2025-09-07 16:51:20 +0530

    added 'back to top' button in all the pages with certain features (decribed in detail in the pr)

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import BackToTopButton from "@/components/BackToTopButton";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +32,7 @@ export default function RootLayout({ children }) {
         <Navbar />
         {children}
         <Footer />
+        <BackToTopButton />
       </body>
     </html>
   );
